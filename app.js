88c9f4565de46438e901aba2e866e876310a6c7a
Sebastian Bastidas <sbastidasr@gmail.com>
2018-05-23 14:12:26 -0500

    Results

--- app.js
+++ app.js
@@ -1,47 +1,53 @@
-//creates request object and response
-funtion getDataFromYelp(searchTerm, callback) {
-  const query = {
-    url: " ",
-    data: {
-      q: '',
-      maxResults: 10,
-    },
-    dataType: 'json',
-    type: 'GET',
-    success: callback,
-    // error: function(error) {console.log(error)},
-    // key: " ",
-  };
-  $.ajax(query)
+for (var i = 0; i < 10; i++) {
+  $(".results-data").append($("#repeat").clone());
 }
 
-//function to render the results to HTML
-function renderQueryResults(results) {
-
-}
-
-//function to place data on page
-function displaySearchData(data) {
-  // renderQueryResults();
-}
-
+// //creates request object and response
+// funtion getDataFromYelp(searchTerm, callback) {
+//   const query = {
+//     url: " ",
+//     data: {
+//       q: '',
+//       maxResults: 10,
+//     },
+//     dataType: 'json',
+//     type: 'GET',
+//     success: callback,
+//     // error: function(error) {console.log(error)},
+//     // key: " ",
+//   };
+//   $.ajax(query)
+// }
 //
-function watchSearchButton(){
-  $('').click(function(event) {
-    event.preventDefault();
-    // const queryTarget = $(event.currentTarget).find();
-    // const query = queryTarget.val();
+// //function to render the results to HTML
+// function renderQueryResults(results) {
+//
+// }
+//
+// //function to place data on page
+// function displaySearchData(data) {
+//   // renderQueryResults();
+// }
+//
+// //
+// function watchSearchButton(){
+//   $('').click(function(event) {
+//     event.preventDefault();
+//     // const queryTarget = $(event.currentTarget).find();
+//     // const query = queryTarget.val();
+//
+//
+//     getDataFromYelp(query, displaySearchData);
+//   })
+// }
+//
+// //document ready
+// $(watchSearchButton);
+//
+// //google API required constructor function to create map object and center it
+// let map = new google.maps.Map(document.getElementById('map'), {
+//   center: {lat: -34.397, lng: 150.644},
+//   zoom: 10
+// });
 
-
-    getDataFromYelp(query, displaySearchData);
-  })
-}
-
-//document ready
-$(watchSearchButton);
-
-//google API required constructor function to create map object and center it
-let map = new google.maps.Map(document.getElementById('map'), {
-  center: {lat: -34.397, lng: 150.644},
-  zoom: 10
-});
+function initMap() {}
